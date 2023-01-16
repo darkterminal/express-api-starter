972f4abd1b1a0c0301e943256f7bdd70622130fd
Imam Ali Mustofa <d.darkterminal@gmail.com>
2023-01-16 03:44:32 +0000

    chore: change to es6 version

--- src/app.js
+++ src/app.js
@@ -1,29 +1,29 @@
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
+import express from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import cors from 'cors'
 
-require('dotenv').config();
+import 'dotenv/config'
 
-const middlewares = require('./middlewares');
-const api = require('./api');
+import middlewares from './middlewares'
+import api from './api'
 
-const app = express();
+const app = express()
 
-app.use(morgan('dev'));
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.json({
     message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄'
-  });
-});
+  })
+})
 
-app.use('/api/v1', api);
+app.use('/api/v1', api)
 
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler);
+app.use(middlewares.notFound)
+app.use(middlewares.errorHandler)
 
-module.exports = app;
+export default app
